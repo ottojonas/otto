ed352d64d2ed2a796415e31f443a0295a53aa393
ottojonas <ottobjonas@outlook.com>
2025-02-07 13:07:34 +0000

    update formatting

--- script.js
+++ script.js
@@ -1,36 +1,8 @@
-const links = [
-  "GitHub → https://github.com/ottojonas",
-  "Discord -> https://discord.com/users/1245513579881304108",
-];
+document.addEventListener("DOMContentLoaded", () => {
+  const menuToggle = document.querySelector(".menu-toggle");
+  const navLinks = document.querySelector(".nav-links");
 
-let i = 0;
-let j = 0;
-let currentText = "";
-let isDeleting = false;
-const output = document.getElementById("social-output");
-
-function typeEffect() {
-  if (i < links.length) {
-    if (!isDeleting && j <= links[i].length) {
-      currentText = links[i].substring(0, j++);
-    } else if (isDeleting && j >= 0) {
-      currentText = links[i].substring(0, j--);
-    }
-
-    output.textContent = currentText;
-
-    if (!isDeleting && j === links[i].length) {
-      isDeleting = true;
-      setTimeout(typeEffect, 3000);
-    } else if (isDeleting && j === 0) {
-      isDeleting = false;
-      i = (i + 1) % links.length;
-      setTimeout(typeEffect, 500);
-    } else {
-      setTimeout(typeEffect, isDeleting ? 50 : 100);
-    }
-  }
-}
-
-document.addEventListener("DOMContentLoaded", typeEffect);
-
+  menuToggle.addEventListener("click", () => {
+    navLinks.classList.toggle("show");
+  });
+});
